5e8be1ef6b3b4b3ab0e91bcee2732d524f9696f4
Benjamin Aho <Benjamin.aho27@gmail.com>
2022-10-03 12:18:09 -0400

    files added

---
+++ data_1_checks.py
@@ -0,0 +1,12 @@
+print("hello world of insanity")
+
+my_list = [1, 2, 2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 2, 3, 4, 2, 2, 2, 3, 4, 2, 2, 2, 3, 4]
+my_list.append(222222222)
+itemTwo = my_list[2]
+print(itemTwo)
+
+my_dictionary = {"a" : "b", "b" : "c", "help" : "how is this confusing?"}
+print(my_dictionary["a"])
+
+my_tuple = ("Noun", "is", "a", "person", "place", "thing")
+print(my_tuple[3])
