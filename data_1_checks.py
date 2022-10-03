033b1a0e31b930b9c591ac2f38f57ac95ae0191e
Benjamin Aho <Benjamin.aho27@gmail.com>
2022-10-03 12:22:14 -0400

    corrected title to reflect assignment

--- data_1_checks.py
+++ data_1_checks.py
@@ -1,11 +1,11 @@
-print("hello world of insanity")
+print("hello world")
 
 my_list = [1, 2, 2, 2, 2, 2, 2, 3, 4, 2, 2, 2, 2, 3, 4, 2, 2, 2, 3, 4, 2, 2, 2, 3, 4]
 my_list.append(222222222)
 itemTwo = my_list[2]
 print(itemTwo)
 
-my_dictionary = {"a" : "b", "b" : "c", "help" : "how is this confusing?"}
+my_dictionary = {"a" : "b", "b" : "c"}
 print(my_dictionary["a"])
 
 my_tuple = ("Noun", "is", "a", "person", "place", "thing")
